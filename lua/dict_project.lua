7d234a63121f493e65cc3489a1bdcb6f3ad60a3a
Hyi <86065391+HEyashdjkasgdkzxjhck@users.noreply.github.com>
2026-04-29 15:24:12 +0800

    Add files via upload
    分词库版本+记录未知词

---
+++ lua/dict_project.lua
@@ -0,0 +1,11 @@
+return {
+    ["蓝莓"] = "Blueberry",
+    ["楼栋"] = "Building",
+    ["能耗"] = "EnergyConsumption",
+    ["用电"] = "PowerUsage",
+    ["电费"] = "ElectricityCost",
+    ["消防主机"] = "FireAlarmPanel",
+    ["充电桩"] = "ChargingPile",
+    ["地下车库"] = "UndergroundParking",
+    ["摄像头"] = "Camera",
+}
